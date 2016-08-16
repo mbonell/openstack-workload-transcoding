f794136f28fd9f861236aab537a7cf9a149678d1
Marcela Bonell <marcelabonell@gmail.com>
2016-08-16 17:56:14 -0500

    Manager microservice HOT Adding the Heat template and init script to launch the manager microservice.

---
+++ heat/manager/init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+REPOSITORY_URL=https://github.com/obazavil/openstack-workload-transcoding.git
+APP_DIR=github.com/obazavil/openstack-workload-transcoding
+
+sudo apt-get update
+sudo apt-get install git -y
+
+# Installing GoLang environment
+sudo curl -O https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.7.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Setting up GoLang workspace
+mkdir $HOME/go-workspace
+mkdir $HOME/go-workspace/bin
+mkdir $HOME/go-workspace/pkg
+mkdir $HOME/go-workspace/src
+export GOPATH=$HOME/go-workspace
+
+# Installing required libraries
+cd $GOPATH/src
+go get github.com/go-kit/kit/log
+go get golang.org/x/net/context
+go get github.com/go-resty/resty
+go get gopkg.in/mgo.v2
+go get github.com/gorilla/mux
+go get github.com/rackspace/gophercloud
+
+# Downloading the code application and running de manager microservice
+mkdir -p $APP_DIR
+git clone $REPOSITORY_URL $APP_DIR
+cd $APP_DIR
+go run transcoding/manager/cmd/main.go
